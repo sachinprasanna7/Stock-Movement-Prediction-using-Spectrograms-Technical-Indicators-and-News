2d0cb11c24772edbc6f4e9d150e53301cb43a95c
Subhojit <ksubhojit20@gmail.com>
2023-11-16 19:00:01 +0530

    17-11-23 speakers added

---
+++ OneDrive/Desktop/tedxnitksurathkal.github.io-master/assets/js/team-list.js
@@ -0,0 +1,121 @@
+var team = [
+    {
+        "name": "Prof. Bhallamudi Ravi",
+        "position": "Director",
+        "image": "NewDirector.jpeg",
+        "linkedin": "",
+        "instagram": "tedxnitksurathkal"
+    },
+    {
+        "name": "Prof. Ampar Chitharanjan Hegde",
+        "position": "Dean - Student Welfare",
+        "image": "DSW.jpg",
+        "linkedin": "",
+        "instagram": "tedxnitksurathkal"
+    },
+    {
+        "name": "Dr. Alwyn Roshan Pais",
+        "position": "Associate Dean - Student Welfare",
+        "image": "ADSW1.jpg",
+        "linkedin": "",
+        "instagram": "tedxnitksurathkal"
+    },
+    {
+        "name": "Dr. C. P. Devatha",
+        "position": "Associate Dean - Student Welfare",
+        "image": "ADSW2.jpeg",
+        "linkedin": "",
+        "instagram": "tedxnitksurathkal"
+    },
+    {
+        "name": "Prof. Prabu K",
+        "position": "Faculty Advisor",
+        "image": "Prabu.jpg",
+        "linkedin": "",
+        "instagram": "tedxnitksurathkal"
+    },
+    {
+        "name": "Ranjana Kambhammettu",
+        "position": "Team Lead",
+        "image": "Ranjana.jpg",
+        "linkedin": "https://www.linkedin.com/in/ranjanakambhammettu/",
+        "instagram": ""
+    },
+    {
+        "name": "Advaith Curpod",
+        "position": "Advisor",
+        "image": "Advaith.jpg",
+        "linkedin": "https://www.linkedin.com/in/advaith-curpod/",
+        "instagram": ""
+    },
+    {
+        "name": "Devvrat Ashtaputre",
+        "position": "Advisor",
+        "image": "Devvrat.jpeg",
+        "linkedin": "https://www.linkedin.com/in/devvrat-ashtaputre-839a13201/",
+        "instagram": ""
+    },
+    {
+        "name": "Vibhu Varshney",
+        "position": "Head Of Curation",
+        "image": "Vibhu.jpg",
+        "linkedin": "https://www.linkedin.com/in/vibhuvarshney081103/",
+        "instagram": ""
+    },
+    {
+        "name": "Vartika Rao",
+        "position": "Head Of Curation",
+        "image": "Vartika.jpg",
+        "linkedin": "https://www.linkedin.com/in/vartika-t-rao/",
+        "instagram": ""
+    },
+    {
+        "name": "Punith Raj",
+        "position": "Head Of Production",
+        "image": "Punith.jpg",
+        "linkedin": "https://www.linkedin.com/in/punith-raj-836926209/",
+        "instagram": ""
+    },
+    {
+        "name": "Rohit Chabbi",
+        "position": "Head Of Production",
+        "image": "Rohit.jpg",
+        "linkedin": "https://www.linkedin.com/in/rohit-chabbi/",
+        "instagram": ""
+    },
+    {
+        "name": "Abhishek P",
+        "position": "Head Of Operations",
+        "image": "Abhishek.jpeg",
+        "linkedin": "https://www.linkedin.com/in/abhishek-p-285b05218/",
+        "instagram": ""
+    },
+    {
+        "name": "Pulkit Jain",
+        "position": "Head Of Operations",
+        "image": "Pulkit.jpg",
+        "linkedin": "https://www.linkedin.com/in/pulkitjain0151/",
+        "instagram": ""
+    },
+    {
+        "name": "Hemanth Somanna",
+        "position": "Head Of Marketing",
+        "image": "Hemanth .jpg",
+        "linkedin": "https://www.linkedin.com/in/hemanth-somanna-b53310208/",
+        "instagram": ""
+    },
+    {
+        "name": "Violina Doley",
+        "position": "Web Manager",
+        "image": "Violina.jpg",
+        "linkedin": "https://www.linkedin.com/in/violinadoley/",
+        "instagram": ""
+    },
+    {
+        "name": "Poorvi Joshi",
+        "position": "Creative Head",
+        "image": "Poorvi.jpg",
+        "linkedin": "https://www.linkedin.com/in/poorvijoshi/",
+        "instagram": ""
+    },
+]
